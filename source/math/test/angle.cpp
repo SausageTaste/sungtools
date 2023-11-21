f48112877a0b4b36c3c1522a61759e44b743372c
Sungmin Woo <woos8899@gmail.com>
2023-11-21 09:48:26 +0900

    Addition test

--- source/math/test/angle.cpp
+++ source/math/test/angle.cpp
@@ -8,6 +8,7 @@
 using sung::Angle;
 
 
+// Randomized test
 namespace {
 
     constexpr double EPSILON = 1e-8;
@@ -16,8 +17,14 @@ namespace {
     enum TestResult {
         TEST_RESULT_SUCCESS = 0,
         TEST_RESULT_TEST_DIFF_FAIL,
+        TEST_RESULT_TEST_ADD_FAIL,
     };
 
+}
+
+
+// Randomized test
+namespace {
 
     class RandomDoubleGenerator {
 
@@ -59,29 +66,85 @@ namespace {
         return true;
     }
 
+
+    TestResult randomized_test() {
+        ::RandomDoubleGenerator rng{ -1e6, 1e6 };
+
+        constexpr auto TEST_DURATION = std::chrono::seconds(1);
+        const auto start_time = std::chrono::high_resolution_clock::now();
+        size_t count = 0;
+
+        for (;;) {
+            const auto a = Angle::from_rad(rng.gen());
+            const auto b = Angle::from_rad(rng.gen());
+
+            if (!test_diff(a, b))
+                return TEST_RESULT_TEST_DIFF_FAIL;
+
+            ++count;
+            if (std::chrono::high_resolution_clock::now() - start_time > TEST_DURATION)
+                break;
+        }
+
+        const auto seconds = std::chrono::duration_cast<std::chrono::milliseconds>(TEST_DURATION).count() / 1000.0;
+        std::cout << "Randomized test passed " << std::fixed << count / seconds << " times per seconds" << std::endl;
+        return TEST_RESULT_SUCCESS;
+    }
+
+}
+
+
+// Addition test
+namespace {
+
+    // All in degrees
+    struct AdditionTestCase {
+        double a_;
+        double b_;
+        double sum_;
+    };
+
+
+    TestResult addition_test() {
+        const std::vector<AdditionTestCase> test_cases{
+            {20, 60, 80},
+            {370, 230, 240},
+            {370, 230, -120},
+            {15.47, -84.354, -68.884},
+        };
+
+        for (auto& test_case : test_cases) {
+            const auto a = Angle::from_deg(test_case.a_);
+            const auto b = Angle::from_deg(test_case.b_);
+            const auto sum = a + b;
+
+            if (!sum.is_equivalent(Angle::from_deg(test_case.sum_), ::EPSILON)) {
+                std::cout << "Test `addition_test` failed"
+                    << "\n    a: " << a.deg()
+                    << "\n    b: " << b.deg()
+                    << "\n    sum: " << sum.deg()
+                    << "\n    expected sum: " << test_case.sum_
+                    << std::endl;
+                return TEST_RESULT_TEST_ADD_FAIL;
+            }
+        }
+
+        return TEST_RESULT_SUCCESS;
+    }
+
 }
 
 
 int main() {
-    ::RandomDoubleGenerator rng{ -1e6, 1e6 };
+    TestResult result;
 
-    constexpr auto TEST_DURATION = std::chrono::seconds(1);
-    const auto start_time = std::chrono::high_resolution_clock::now();
-    size_t count = 0;
+    result = ::addition_test();
+    if (TEST_RESULT_SUCCESS != result)
+        return result;
 
-    for (;;) {
-        const auto a = Angle::from_rad(rng.gen());
-        const auto b = Angle::from_rad(rng.gen());
+    result = ::randomized_test();
+    if (TEST_RESULT_SUCCESS != result)
+        return result;
 
-        if (!test_diff(a, b))
-            return TEST_RESULT_TEST_DIFF_FAIL;
-
-        ++count;
-        if (std::chrono::high_resolution_clock::now() - start_time > TEST_DURATION)
-            break;
-    }
-
-    const auto seconds = std::chrono::duration_cast<std::chrono::milliseconds>(TEST_DURATION).count() / 1000.0;
-    std::cout << "Test passed " << std::fixed << count / seconds << " times per seconds" << std::endl;
-    return TEST_RESULT_SUCCESS;
+    return 0;
 }
