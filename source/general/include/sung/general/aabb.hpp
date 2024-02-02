b3a23013da79e527b98f5bf7e3f6c9736507e4bd
Sungmin Woo <woos8899@gmail.com>
2024-02-02 10:53:06 +0900

    Find intersection of AABB2

--- source/general/include/sung/general/aabb.hpp
+++ source/general/include/sung/general/aabb.hpp
@@ -21,6 +21,9 @@ namespace sung {
 
         T width() const { return x_max_ - x_min_; }
         T height() const { return y_max_ - y_min_; }
+        T area() const {
+            return this->width() * this->height();
+        }
 
         // Returns true if the point is inside the volume, not on the surface of it
         bool is_inside(T x, T y) const {
@@ -30,6 +33,25 @@ namespace sung {
         // Returns true if the point is inside the volume or on the surface of it
         bool is_contacting(T x, T y) const {
             return x >= x_min_ && x <= x_max_ && y >= y_min_ && y <= y_max_;
+        }
+
+        bool make_intersection(const AABB2& other, AABB2& output) const {
+            if (x_max_ < other.x_min_ || x_min_ > other.x_max_ || y_max_ < other.y_min_ || y_min_ > other.y_max_)
+                return false;
+
+            output.x_min_ = std::max(x_min_, other.x_min_);
+            output.x_max_ = std::min(x_max_, other.x_max_);
+            output.y_min_ = std::max(y_min_, other.y_min_);
+            output.y_max_ = std::min(y_max_, other.y_max_);
+            return true;
+        }
+
+        double sharing_area(const AABB2& other) const {
+            AABB2 intersection;
+            if (!this->make_intersection(other, intersection))
+                return 0;
+
+            return intersection.area();
         }
 
         // Yes it makes the volume 0
