a5458124b3fc70358f23ca3cb8bf19834dd079bf
Sungmin Woo <woos8899@gmail.com>
2024-03-12 17:09:33 +0900

    `AABB3`

--- source/general/include/sung/general/aabb.hpp
+++ source/general/include/sung/general/aabb.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 
+#include "linalg.hpp"
 #include "mamath.hpp"
 #include "optional.hpp"
 
@@ -16,6 +17,23 @@ namespace sung {
         constexpr AABB1() = default;
         constexpr AABB1(T val0, T val1) {
             this->set(val0, val1);
+        }
+
+        constexpr AABB1 operator+(T val) const {
+            return AABB1{ min_ + val, max_ + val };
+        }
+        constexpr AABB1 operator-(T val) const {
+            return AABB1{ min_ - val, max_ - val };
+        }
+        constexpr AABB1& operator+=(T val) {
+            min_ += val;
+            max_ += val;
+            return *this;
+        }
+        constexpr AABB1& operator-=(T val) {
+            min_ -= val;
+            max_ -= val;
+            return *this;
         }
 
         constexpr T mini() const { return min_; }
@@ -107,8 +125,8 @@ namespace sung {
         }
 
         constexpr T x_min() const { return x_.mini(); }
-        constexpr T y_min() const { return y_.mini(); }
         constexpr T x_max() const { return x_.maxi(); }
+        constexpr T y_min() const { return y_.mini(); }
         constexpr T y_max() const { return y_.maxi(); }
 
         constexpr T width() const { return x_.len(); }
@@ -167,6 +185,11 @@ namespace sung {
             y_.set(y0, y1);
         }
 
+        constexpr void add(T x, T y) {
+            x_.add(x);
+            y_.add(y);
+        }
+
         constexpr void expand_to_span(T x, T y) {
             x_.expand_to_span(x);
             y_.expand_to_span(y);
@@ -179,6 +202,163 @@ namespace sung {
 
     private:
         AABB1<T> x_, y_;
+
+    };
+
+
+    template <typename T>
+    class AABB3 {
+
+    public:
+        constexpr AABB3() = default;
+        constexpr AABB3(T x0, T x1, T y0, T y1, T z0, T z1) {
+            this->set(x0, x1, y0, y1, z0, z1);
+        }
+        constexpr AABB3(const TVec3<T>& min, const TVec3<T>& max) {
+            this->set(min, max);
+        }
+
+        constexpr AABB3 operator+(const TVec3<T>& v) const {
+            return AABB3{ x_ + v.x(), y_ + v.y(), z_ + v.z() };
+        }
+        constexpr AABB3 operator-(const TVec3<T>& v) const {
+            return AABB3{ x_ - v.x(), y_ - v.y(), z_ - v.z() };
+        }
+        constexpr AABB3& operator+=(const TVec3<T>& v) {
+            x_ += v.x();
+            y_ += v.y();
+            z_ += v.z();
+            return *this;
+        }
+        constexpr AABB3& operator-=(const TVec3<T>& v) {
+            x_ -= v.x();
+            y_ -= v.y();
+            z_ -= v.z();
+            return *this;
+        }
+
+        constexpr T x_min() const { return x_.mini(); }
+        constexpr T x_max() const { return x_.maxi(); }
+        constexpr T y_min() const { return y_.mini(); }
+        constexpr T y_max() const { return y_.maxi(); }
+        constexpr T z_min() const { return z_.mini(); }
+        constexpr T z_max() const { return z_.maxi(); }
+
+        constexpr TVec3<T> mini() const {
+            return TVec3<T>{ this->x_min(), this->y_min(), this->z_min() };
+        }
+        constexpr TVec3<T> maxi() const {
+            return TVec3<T>{ this->x_max(), this->y_max(), this->z_max() };
+        }
+
+        constexpr T x_len() const { return x_.len(); }
+        constexpr T y_len() const { return y_.len(); }
+        constexpr T z_len() const { return z_.len(); }
+        constexpr T volume() const {
+            return this->x_len() * this->y_len() * this->z_len();
+        }
+
+        constexpr bool are_similar(const AABB3& rhs, T epsilon) const {
+            return x_.are_similar(rhs.x_, epsilon)
+                && y_.are_similar(rhs.y_, epsilon)
+                && z_.are_similar(rhs.z_, epsilon);
+        }
+
+        constexpr bool is_inside_op(T x, T y, T z) const {
+            return x_.is_inside_op(x)
+                && y_.is_inside_op(y)
+                && z_.is_inside_op(z);
+        }
+        constexpr bool is_inside_op(const TVec3<T>& point) const {
+            return this->is_inside_op(point.x(), point.y(), point.z());
+        }
+        constexpr bool is_inside_cl(T x, T y, T z) const {
+            return x_.is_inside_cl(x)
+                && y_.is_inside_cl(y)
+                && z_.is_inside_cl(z);
+        }
+        constexpr bool is_inside_cl(const TVec3<T>& point) const {
+            return this->is_inside_cl(point.x(), point.y(), point.z());
+        }
+
+        constexpr bool is_intersecting_op(const AABB3& rhs) const {
+            return x_.is_intersecting_op(rhs.x_)
+                && y_.is_intersecting_op(rhs.y_)
+                && z_.is_intersecting_op(rhs.z_);
+        }
+        constexpr bool is_intersecting_cl(const AABB3& rhs) const {
+            return x_.is_intersecting_cl(rhs.x_)
+                && y_.is_intersecting_cl(rhs.y_)
+                && z_.is_intersecting_cl(rhs.z_);
+        }
+
+        constexpr bool make_intersection(const AABB3& rhs, AABB3& out) const {
+            if (auto x_intersection = x_.make_intersection(rhs.x_)) {
+                if (auto y_intersection = y_.make_intersection(rhs.y_)) {
+                    if (auto z_intersection = z_.make_intersection(rhs.z_)) {
+                        out.x_ = *x_intersection;
+                        out.y_ = *y_intersection;
+                        out.z_ = *z_intersection;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        constexpr Optional<AABB3> make_intersection(const AABB3& rhs) const {
+            AABB3 output;
+            if (this->make_intersection(rhs, output))
+                return output;
+            else
+                return sung::nullopt;
+        }
+
+        constexpr void set(T x, T y, T z) {
+            x_.set(x);
+            y_.set(y);
+            z_.set(y);
+        }
+        constexpr void set(const TVec3<T>& point) {
+            this->set(point.x(), point.y(), point.z());
+        }
+        constexpr void set(T x0, T x1, T y0, T y1, T z0, T z1) {
+            x_.set(x0, x1);
+            y_.set(y0, y1);
+            z_.set(z0, z1);
+        }
+        constexpr void set(const TVec3<T>& min, const TVec3<T>& max) {
+            this->set(min.x(), max.x(), min.y(), max.y(), min.z(), max.z());
+        }
+
+        constexpr add(T x, T y, T z) {
+            x_.add(x);
+            y_.add(y);
+            z_.add(z);
+        }
+        constexpr add(const TVec3<T>& point) {
+            this->add(point.x(), point.y(), point.z());
+        }
+
+        constexpr void expand_to_span(T x, T y, T z) {
+            x_.expand_to_span(x);
+            y_.expand_to_span(y);
+            z_.expand_to_span(z);
+        }
+        constexpr void expand_to_span(const TVec3<T>& point) {
+            this->expand_to_span(point.x(), point.y(), point.z());
+        }
+        constexpr AABB3 get_expanded_to_span(T x, T y, T z) const {
+            AABB3 output = *this;
+            output.expand_to_span(x, y, z);
+            return output;
+        }
+        constexpr AABB3 get_expanded_to_span(const TVec3<T>& point) const {
+            return this->get_expanded_to_span(point.x(), point.y(), point.z());
+        }
+
+    private:
+        AABB1<T> x_, y_, z_;
 
     };
 
--- source/general/test/aabb.cpp
+++ source/general/test/aabb.cpp
@@ -57,6 +57,47 @@ namespace {
         return 0;
     }
 
+    template <typename T>
+    int test_aabb3() {
+        using Vec3 = sung::TVec3<T>;
+        using AABB3 = sung::AABB3<T>;
+
+        constexpr AABB3 a{ 2, -1, 3, 5, 7, 11 };
+        constexpr AABB3 b{ Vec3{-2, 7, 5}, Vec3{1, 3, 13} };
+
+        static_assert(a.x_min() == -1, "AABB3::x_min failed");
+        static_assert(a.y_min() == 3, "AABB3::y_min failed");
+        static_assert(a.z_min() == 7, "AABB3::z_min failed");
+        static_assert(a.x_max() == 2, "AABB3::x_max failed");
+        static_assert(a.y_max() == 5, "AABB3::y_max failed");
+        static_assert(a.z_max() == 11, "AABB3::z_max failed");
+
+        static_assert(a.x_len() == 3, "AABB3::x_len failed");
+        static_assert(a.y_len() == 2, "AABB3::y_len failed");
+        static_assert(a.z_len() == 4, "AABB3::z_len failed");
+        static_assert(a.volume() == 24, "AABB3::volume failed");
+
+        static_assert(a.is_inside_cl(Vec3{ 2, 5, 11 }), "AABB3::is_inside_cl failed");
+        static_assert(!a.is_inside_op(Vec3{ 2, 5, 11 }), "AABB3::is_inside_op failed");
+        static_assert(!a.is_inside_cl(Vec3{ 2, 5, 12 }), "AABB3::is_inside_cl failed");
+        static_assert(!a.is_inside_op(Vec3{ 2, 5, 12 }), "AABB3::is_inside_op failed");
+
+        constexpr auto inter1 = a.make_intersection(b);
+        static_assert(
+            inter1->are_similar(AABB3{ -1, 1, 3, 5, 7, 11 }, 0),
+            "AABB3::make_intersection failed"
+        );
+
+        constexpr auto a_expanded = a.get_expanded_to_span(Vec3{ 12, -12, 0 });
+        static_assert(
+            a_expanded.are_similar(AABB3{ 12, -1, -12, 5, 0, 11 }, (T)0.001),
+            "AABB3::get_expanded_to_span failed"
+        );
+
+        return 0;
+
+    }
+
 }
 
 
@@ -74,6 +115,14 @@ int main() {
         return result;
 
     result = ::test_aabb2<double>();
+    if (0 != result)
+        return result;
+
+    result = ::test_aabb3<float>();
+    if (0 != result)
+        return result;
+
+    result = ::test_aabb3<double>();
     if (0 != result)
         return result;
 
