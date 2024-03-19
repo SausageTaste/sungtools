fc6035a2eefed163dfdbdaeefe22ae4e9726b0f7
Sungmin Woo <woos8899@gmail.com>
2024-03-19 12:44:51 +0900

    Adapt googletest

--- source/general/test/angle.cpp
+++ source/general/test/angle.cpp
@@ -1,23 +1,12 @@
-#include "sung/general/angle.hpp"
-
 #include <chrono>
-#include <iostream>
 #include <random>
 
-#include "sung/general/time.hpp"
+#include <gtest/gtest.h>
+
+#include "sung/general/angle.hpp"
 
 
 namespace {
-
-    enum TestResult {
-        TEST_RESULT_SUCCESS = 0,
-        TEST_RESULT_TEST_DIFF_FLOAT_FAIL,
-        TEST_RESULT_TEST_DIFF_DOUBLE_FAIL,
-        TEST_RESULT_TEST_DIFF_FUNC_FAIL,
-        TEST_RESULT_TEST_ADD_FAIL,
-        TEST_RESULT_TEST_CONVERSION_FAIL,
-    };
-
 
     template <typename T>
     class RandomDoubleGenerator {
@@ -25,9 +14,8 @@ namespace {
     public:
         RandomDoubleGenerator(T lower_bound, T upper_bound)
             : unif_(lower_bound, upper_bound) {
-            const auto time_seed = std::chrono::high_resolution_clock::now()
-                                       .time_since_epoch()
-                                       .count();
+            const auto now = std::chrono::high_resolution_clock::now();
+            const auto time_seed = now.time_since_epoch().count();
             std::seed_seq ss{ uint32_t(time_seed & 0xffffffff),
                               uint32_t(time_seed >> 32) };
             rng_.seed(ss);
@@ -46,266 +34,56 @@ namespace {
 // Diff calculation test
 namespace {
 
-    template <typename T>
-    void print_diff_test_variables(
-        const sung::TAngle<T> a, const sung::TAngle<T> b
-    ) {
-        const auto d = a.calc_short_diff_to(b);
-        const auto a_plus_d = a + d;
-        const auto b_ad_diff = b.calc_short_diff_to(a_plus_d);
-
-        std::cout << "Test `randomized_test` failed (T = " << typeid(T).name()
-                  << ")"
-                  << "\n    a:              " << a.rad() << " ("
-                  << a.normalize_neg().rad() << ")"
-                  << "\n    b:              " << b.rad() << " ("
-                  << b.normalize_neg().rad() << ")"
-                  << "\n    d:              " << d.rad() << " ("
-                  << d.normalize_neg().rad() << ")"
-                  << "\n    a+d:            " << a_plus_d.rad() << " ("
-                  << a_plus_d.normalize_neg().rad() << ")"
-                  << "\n    Diff of a+d, b: " << b_ad_diff.rad() << " ("
-                  << b_ad_diff.normalize_neg().rad() << ")" << std::endl;
-    }
-
-
-    TestResult diff_calc_test() {
+    TEST(Angle, ShortestDiffCalcEq) {
         ::RandomDoubleGenerator<double> rng{ -1e3, 1e3 };
 
-        constexpr auto TEST_DURATION = std::chrono::seconds(1);
-        const auto start_time = std::chrono::high_resolution_clock::now();
-        size_t count = 0;
-
-        for (;;) {
+        for (int i = 0; i < 100; ++i) {
             const auto a_deg = rng.gen();
             const auto b_deg = rng.gen();
-
-            const auto ad = sung::TAngle<double>::from_rad(a_deg);
-            const auto bd = sung::TAngle<double>::from_rad(b_deg);
-            const auto dd = ad.calc_short_diff_to(bd);
-            const auto a_plus_d_d = ad + dd;
-            if (!bd.is_equivalent(a_plus_d_d, 1e-10)) {
-                ::print_diff_test_variables(ad, bd);
-                return TEST_RESULT_TEST_DIFF_DOUBLE_FAIL;
-            }
-
-            const auto af = sung::TAngle<float>::from_rad(
-                static_cast<float>(a_deg)
-            );
-            const auto bf = sung::TAngle<float>::from_rad(
-                static_cast<float>(b_deg)
-            );
-            const auto df = af.calc_short_diff_to(bf);
-            const auto a_plus_d_f = af + df;
-            if (!bf.is_equivalent(a_plus_d_f, 1e-3f)) {
-                ::print_diff_test_variables(af, bf);
-                return TEST_RESULT_TEST_DIFF_FLOAT_FAIL;
-            }
-
-            ++count;
-            if (std::chrono::high_resolution_clock::now() - start_time >
-                TEST_DURATION)
-                break;
-        }
-
-        const auto seconds =
-            std::chrono::duration_cast<std::chrono::milliseconds>(TEST_DURATION)
-                .count() /
-            1000.0;
-        std::cout << "`diff_calc_test` passed " << std::fixed << count / seconds
-                  << " times per seconds" << std::endl;
-        return TEST_RESULT_SUCCESS;
-    }
-
-    TestResult diff_calc_func_test() {
-        ::RandomDoubleGenerator<double> rng{ -1e3, 1e3 };
-
-        constexpr auto TEST_DURATION = std::chrono::seconds(1);
-        const auto start_time = std::chrono::high_resolution_clock::now();
-        size_t count = 0;
-
-        sung::TimeChecker timer;
-        double accum_mod = 0;
-        double accum_floor = 0;
-
-        for (;;) {
-            const auto a_deg = rng.gen();
-            const auto b_deg = rng.gen();
-
             const auto a_rad = sung::to_radians(a_deg);
             const auto b_rad = sung::to_radians(b_deg);
 
-            timer.check();
-            const auto diff_mod = sung::calc_rad_shortest_diff_mod(
-                a_rad, b_rad
+            EXPECT_DOUBLE_EQ(
+                sung::calc_rad_shortest_diff_mod(a_rad, b_rad),
+                sung::calc_rad_shortest_diff_floor(a_rad, b_rad)
             );
-            accum_mod += timer.elapsed();
+        }
+    }
 
-            timer.check();
-            const auto diff_floor = sung::calc_rad_shortest_diff_floor(
-                a_rad, b_rad
+
+    TEST(Angle, BinaryOperation) {
+        using Angle = sung::TAngle<double>;
+        ::RandomDoubleGenerator<double> rng{ -1e3, 1e3 };
+
+        for (int i = 0; i < 100; ++i) {
+            const auto a_deg = rng.gen();
+            const auto b_deg = rng.gen();
+
+            const auto a = Angle::from_deg(a_deg);
+            const auto b = Angle::from_deg(b_deg);
+
+            EXPECT_NEAR(
+                (a + b).rad(), Angle::from_deg(a_deg + b_deg).rad(), 1e-10
             );
-            accum_floor += timer.elapsed();
+            EXPECT_NEAR(
+                (a - b).rad(), Angle::from_deg(a_deg - b_deg).rad(), 1e-10
+            );
+            EXPECT_NEAR(
+                (a * b_deg).rad(), Angle::from_deg(a_deg).rad() * b_deg, 1e-10
+            );
+            EXPECT_NEAR(
+                (a / b_deg).rad(), Angle::from_deg(a_deg).rad() / b_deg, 1e-10
+            );
 
-            if (!sung::are_similiar(diff_mod, diff_floor, 1e-10)) {
-                std::cout << "Test `diff_calc_func_test` failed (T = "
-                          << typeid(double).name() << ")"
-                          << "\n    a:            " << a_rad << " ("
-                          << sung::to_degrees(a_rad) << ")"
-                          << "\n    b:            " << b_rad << " ("
-                          << sung::to_degrees(b_rad) << ")"
-                          << "\n    diff_mod:     " << diff_mod << " ("
-                          << sung::to_degrees(diff_mod) << ")"
-                          << "\n    diff_floor:   " << diff_floor << " ("
-                          << sung::to_degrees(diff_floor) << ")" << std::endl;
-                return TEST_RESULT_TEST_DIFF_FUNC_FAIL;
-            }
-
-            ++count;
-            if (std::chrono::high_resolution_clock::now() - start_time >
-                TEST_DURATION)
-                break;
+            const auto a2b = a.calc_short_diff_to(b);
+            EXPECT_NEAR(b.calc_short_diff_to(a + a2b).rad(), 0, 1e-10);
         }
-
-        const auto seconds =
-            std::chrono::duration_cast<std::chrono::milliseconds>(TEST_DURATION)
-                .count() /
-            1000.0;
-        std::cout << "`diff_calc_func_test` passed " << std::fixed
-                  << count / seconds << " times per seconds" << std::endl;
-        std::cout << "    mod took " << accum_mod << " seconds, floor took "
-                  << accum_floor << " seconds" << std::endl;
-        return TEST_RESULT_SUCCESS;
     }
 
 }  // namespace
 
 
-// Addition test
-namespace {
-
-    // All in degrees
-    template <typename T>
-    struct AdditionTestCase {
-        T a_;
-        T b_;
-        T sum_;
-    };
-
-
-    template <typename T>
-    TestResult addition_test(T epsilon) {
-        const std::vector<AdditionTestCase<T>> test_cases{
-            { 20, 60, 80 },
-            { 370, 230, 240 },
-            { 370, 230, -120 },
-            { (T)15.47, (T)-84.354, (T)-68.884 },
-        };
-
-        for (auto& test_case : test_cases) {
-            const auto a = sung::TAngle<T>::from_deg(test_case.a_);
-            const auto b = sung::TAngle<T>::from_deg(test_case.b_);
-            const auto sum = a + b;
-
-            if (!sum.is_equivalent(
-                    sung::TAngle<T>::from_deg(test_case.sum_), epsilon
-                )) {
-                std::cout << "Test `addition_test` failed (T = "
-                          << typeid(T).name() << ")"
-                          << "\n    a: " << a.deg() << "\n    b: " << b.deg()
-                          << "\n    sum: " << sum.deg()
-                          << "\n    expected sum: " << test_case.sum_
-                          << std::endl;
-                return TEST_RESULT_TEST_ADD_FAIL;
-            }
-        }
-
-        return TEST_RESULT_SUCCESS;
-    }
-
-}  // namespace
-
-
-// Angle conversion test
-namespace {
-
-    template <typename T>
-    TestResult conversion_test(T error_tolerance) {
-        ::RandomDoubleGenerator<T> rng{ -1e3, 1e3 };
-
-        constexpr auto TEST_DURATION = std::chrono::seconds(1);
-        const auto start_time = std::chrono::high_resolution_clock::now();
-        size_t count = 0;
-
-        for (;;) {
-            const auto deg_value = rng.gen();
-            const auto angle = sung::TAngle<T>::from_deg(deg_value);
-            const auto error = std::abs(angle.deg() - deg_value);
-            if (error > error_tolerance) {
-                std::cout << "Test `conversion_test` failed (T = "
-                          << typeid(T).name() << ")"
-                          << "\n    deg_value: " << deg_value
-                          << "\n    angle.deg(): " << angle.deg()
-                          << "\n    error: " << error << std::endl;
-                return TEST_RESULT_TEST_CONVERSION_FAIL;
-            }
-
-            ++count;
-            if (std::chrono::high_resolution_clock::now() - start_time >
-                TEST_DURATION)
-                break;
-        }
-
-        const auto seconds =
-            std::chrono::duration_cast<std::chrono::milliseconds>(TEST_DURATION)
-                .count() /
-            1000.0;
-        std::cout << "Conversion test (" << typeid(T).name() << ") passed "
-                  << std::fixed << count / seconds << " times per seconds"
-                  << std::endl;
-        return TEST_RESULT_SUCCESS;
-    }
-
-}  // namespace
-
-
-int main() {
-    TestResult result;
-
-    result = ::addition_test<float>(1e-5f);
-    if (TEST_RESULT_SUCCESS != result)
-        return result;
-
-    result = ::addition_test<double>(1e-6);
-    if (TEST_RESULT_SUCCESS != result)
-        return result;
-
-    result = ::diff_calc_test();
-    if (TEST_RESULT_SUCCESS != result)
-        return result;
-
-    result = ::diff_calc_func_test();
-    if (TEST_RESULT_SUCCESS != result)
-        return result;
-
-    result = ::conversion_test<double>(1e-10);
-    if (TEST_RESULT_SUCCESS != result)
-        return result;
-
-    result = ::conversion_test<float>(1e-4f);
-    if (TEST_RESULT_SUCCESS != result)
-        return result;
-
-    {
-        constexpr auto a0 = sung::TAngle<double>::from_deg(-12);
-        constexpr auto a1 = sung::TAngle<double>::from_deg(345);
-
-        constexpr auto a0_pos = a0.normalize_pos().deg();
-        constexpr auto a1_neg = a1.normalize_neg().deg();
-
-        constexpr auto diff = a0.calc_short_diff_to(a1).deg();
-        const auto a0_acos = sung::acos_safe(a0.rad());
-    }
-
-    return 0;
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
