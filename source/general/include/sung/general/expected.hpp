3c599e1930fb3695b9a73cf96c8bb07b2184fa4e
Sungmin Woo <woos8899@gmail.com>
2024-03-19 17:15:25 +0900

    `sung::bad_expected_access`

--- source/general/include/sung/general/expected.hpp
+++ source/general/include/sung/general/expected.hpp
@@ -10,20 +10,24 @@
 namespace sung {
 
 #if defined(__cpp_lib_expected)
-
     #pragma message("[SUNG] std::expected will be used")
 
     template <typename T, typename E>
     using Expected = std::expected<T, E>;
 
+    template <class E>
+    using bad_expected_access = std::bad_expected_access<E>;
+
     using std::unexpected;
 
 #else
-
     #pragma message("[SUNG] tl::expected will be used")
 
     template <typename T, typename E>
     using Expected = tl::expected<T, E>;
+
+    template <class E>
+    using bad_expected_access = tl::bad_expected_access<E>;
 
     using tl::unexpected;
 
