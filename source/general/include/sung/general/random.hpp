3bdc4cdb79f0215953764c28db0e1068508bb2ae
Sungmin Woo <woos8899@gmail.com>
2024-03-22 10:58:14 +0900

    Integer random generator

--- source/general/include/sung/general/random.hpp
+++ source/general/include/sung/general/random.hpp
@@ -7,6 +7,35 @@
 namespace sung {
 
     template <typename T>
+    class RandomIntegerGenerator {
+
+    public:
+        RandomIntegerGenerator(T lower_bound, T upper_bound)
+            : unif_(lower_bound, upper_bound)
+            , lower_bound_(lower_bound)
+            , upper_bound_(upper_bound) {
+            const auto now = std::chrono::high_resolution_clock::now();
+            const auto time_seed = now.time_since_epoch().count();
+            const std::seed_seq ss{ uint32_t(time_seed & 0xffffffff),
+                                    uint32_t(time_seed >> 32) };
+
+            rng_.seed(ss);
+        }
+
+        T lower_bound() const { return lower_bound_; }
+        T upper_bound() const { return upper_bound_; }
+
+        T gen() { return unif_(rng_); }
+
+    private:
+        std::mt19937_64 rng_;
+        std::uniform_int_distribution<T> unif_;
+        T lower_bound_;
+        T upper_bound_;
+    };
+
+
+    template <typename T>
     class RandomRealNumGenerator {
 
     public:
--- source/general/test/random.cpp
+++ source/general/test/random.cpp
@@ -5,6 +5,25 @@
 
 namespace {
 
+    TEST(Random, IntegerGenerator) {
+        using T = int;
+        constexpr int ITERATIONS = 10000;
+        double weighted_sum = 0;
+        sung::RandomIntegerGenerator<T> gen(-11, 37);
+
+        for (int i = 0; i < ITERATIONS; ++i) {
+            const auto val = gen.gen();
+            EXPECT_GE(val, gen.lower_bound());
+            EXPECT_LE(val, gen.upper_bound());
+            weighted_sum += static_cast<double>(val) / ITERATIONS;
+        }
+
+        const auto expected_mean = (gen.upper_bound() + gen.lower_bound()) *
+                                   0.5;
+        EXPECT_NEAR(weighted_sum, expected_mean, 1e-0);
+    }
+
+
     TEST(Random, RealNumberGenerator) {
         using T = float;
         constexpr int ITERATIONS = 10000;
