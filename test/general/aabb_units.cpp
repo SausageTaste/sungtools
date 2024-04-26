b7667147c7b5448d2f98fefd49269551780ab358
Sungmin Woo <woos8899@gmail.com>
2024-04-26 14:57:02 +0900

    Simplify unit aabb test

--- test/general/aabb_units.cpp
+++ test/general/aabb_units.cpp
@@ -7,27 +7,19 @@
 namespace {
 
     TEST(AABB, WithDistance) {
-        using Distance = sung::TDistance<float>;
+        using Dist = sung::TDistance<float>;
 
-        constexpr auto MIN_RANGE = Distance::from_nm(1);
-        constexpr auto MAX_RANGE = Distance::from_nm(2);
-        constexpr sung::AABB1<Distance> aabb(MIN_RANGE, MAX_RANGE);
+        constexpr auto MIN_RANGE = Dist::from_nm(1);
+        constexpr auto MAX_RANGE = Dist::from_nm(2);
+        constexpr sung::AABB1<Dist> aabb(MIN_RANGE, MAX_RANGE);
 
-        static_assert(
-            aabb.is_inside_op(Distance::from_m(2000)), "AABB1 is_inside failed"
-        );
-        static_assert(
-            !aabb.is_inside_op(Distance::from_m(1000)), "AABB1 is_inside failed"
-        );
-        static_assert(
-            aabb.is_inside_cl(MAX_RANGE), "AABB1 is_contacting failed"
-        );
-        static_assert(!aabb.is_inside_op(MAX_RANGE), "AABB1 is_inside failed");
+        static_assert(aabb.is_inside_op(Dist::from_m(2000)), "");
+        static_assert(!aabb.is_inside_op(Dist::from_m(1000)), "");
+        static_assert(aabb.is_inside_cl(MAX_RANGE), "");
+        static_assert(!aabb.is_inside_op(MAX_RANGE), "");
 
-        static_assert(aabb.mid() == Distance::from_nm(1.5), "AABB1 mid failed");
-        static_assert(
-            aabb.len() == Distance::from_nm(1), "AABB1 length failed"
-        );
+        static_assert(aabb.mid() == Dist::from_nm(1.5), "");
+        static_assert(aabb.len() == Dist::from_nm(1), "");
     }
 
 }  // namespace
