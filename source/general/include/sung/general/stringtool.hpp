2ea6ed6fd33a921c6641e9f7c727c4ad406febf8
Sungmin Woo <woos8899@gmail.com>
2024-07-29 15:33:42 +0900

    `format_bytes`

--- source/general/include/sung/general/stringtool.hpp
+++ source/general/include/sung/general/stringtool.hpp
@@ -7,4 +7,6 @@ namespace sung {
 
     std::string slugify(std::string str, const char* replacer = "");
 
-}
+    std::string format_bytes(size_t bytes);
+
+}  // namespace sung
--- source/general/src/stringtool.cpp
+++ source/general/src/stringtool.cpp
@@ -1,6 +1,19 @@
 #include "sung/general/stringtool.hpp"
 
+#include <iomanip>
 #include <regex>
+#include <sstream>
+
+
+namespace {
+
+    std::string format_float(double x, std::streamsize precision = 2) {
+        std::stringstream ss;
+        ss << std::fixed << std::setprecision(precision) << x;
+        return ss.str();
+    }
+
+}  // namespace
 
 
 namespace sung {
@@ -19,6 +32,18 @@ namespace sung {
         str = std::regex_replace(str, std::regex("#-$"), r);
 
         return str;
+    }
+
+    std::string format_bytes(size_t bytes) {
+        if (bytes < 1024) {
+            return ::format_float(static_cast<double>(bytes)) + " B";
+        } else if (bytes < 1024 * 1024) {
+            return ::format_float(bytes / 1024.0) + " KiB";
+        } else if (bytes < 1024 * 1024 * 1024) {
+            return ::format_float(bytes / (1024.0 * 1024.0)) + " MiB";
+        } else {
+            return ::format_float(bytes / (1024.0 * 1024.0 * 1024.0)) + " GiB";
+        }
     }
 
 }  // namespace sung
