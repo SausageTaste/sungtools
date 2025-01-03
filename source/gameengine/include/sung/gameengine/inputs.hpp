581e35934aaec80fdcf81366fab5899ae8d97e3a
Sungmin Woo <woos8899@gmail.com>
2025-01-03 13:36:02 +0900

    Game engine module

---
+++ source/gameengine/include/sung/gameengine/inputs.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <array>
+#include <chrono>
+
+#include "sung/general/optional.hpp"
+
+
+namespace sung { namespace key {
+
+    enum class ActionType { down, up };
+
+    // clang-format off
+    enum class KeyCode {
+        /* Alphabets */
+        a, b, c, d, e, f, g, h, i, j, k, l, m,
+        n, o, p, q, r, s, t, u, v, w, x, y, z,
+        /* Horizontal numbers */
+        n0, n1, n2, n3, n4, n5, n6, n7, n8, n9,
+        /* Misc in keyboard main area */
+        backquote, minus, equal, lbracket, rbracket, backslash, semicolon,
+        quote, comma, period, slash,
+        /* Special characters */
+        space, enter, backspace, tab,
+        /* No characters */
+        escape, lshfit, rshfit, lctrl, rctrl, lalt, ralt, up, down, left, right,
+        /* End Of Enum, just for calculating number of elements of Enum class */
+        eoe
+    };
+    // clang-format on
+
+
+    struct Event {
+        using Clock_t = std::chrono::steady_clock;
+
+        Clock_t::time_point tp_ = Clock_t::now();
+        ActionType action_ = ActionType::down;
+        KeyCode key_ = KeyCode::eoe;
+    };
+
+
+    class EventAnalyzer {
+
+    private:
+        using Clock_t = Event::Clock_t;
+
+        struct KeyState {
+            Clock_t::time_point tp_ = Clock_t::now();
+            bool pressed = false;
+        };
+
+    public:
+        void notify(const Event& e);
+        bool is_pressed(KeyCode key) const;
+        sung::Optional<Clock_t::time_point> get_timepoint(KeyCode key) const;
+
+    private:
+        static size_t convert_key_to_index(const KeyCode key);
+
+        constexpr static auto KEYSPEC_SIZE = static_cast<size_t>(KeyCode::eoe);
+        std::array<KeyState, KEYSPEC_SIZE> states_;
+    };
+
+}}  // namespace sung::key
+
+
+namespace sung { namespace mouse {
+
+    enum class ActionType { move, down, up, mwheel_up, mwheel_down };
+
+    enum class ButtonCode { left, right, middle, unkown };
+
+
+    struct Event {
+        using Clock_t = std::chrono::steady_clock;
+
+        Clock_t::time_point timepoint_ = Clock_t::now();
+        ActionType action_ = ActionType::down;
+        ButtonCode button_ = ButtonCode::unkown;
+        int32_t btn_id_ = -1;
+        double xpos_ = 0;
+        double ypos_ = 0;
+    };
+
+}}  // namespace sung::mouse
---
+++ source/gameengine/src/inputs.cpp
@@ -0,0 +1,36 @@
+#include "sung/gameengine/inputs.hpp"
+
+
+namespace sung { namespace key {
+
+    void EventAnalyzer::notify(const Event& e) {
+        const auto index = static_cast<size_t>(e.key_);
+        if (index >= states_.size())
+            return;
+
+        states_[index].tp_ = e.tp_;
+        states_[index].pressed = (e.action_ == key::ActionType::down);
+    }
+
+    bool EventAnalyzer::is_pressed(KeyCode key) const {
+        const auto index = static_cast<size_t>(key);
+        if (index >= states_.size())
+            return false;
+
+        return states_[index].pressed;
+    }
+
+    sung::Optional<EventAnalyzer::Clock_t::time_point>
+    EventAnalyzer::get_timepoint(KeyCode key) const {
+        const auto index = static_cast<size_t>(key);
+        if (index >= states_.size())
+            return sung::nullopt;
+
+        return states_[index].tp_;
+    }
+
+    size_t EventAnalyzer::convert_key_to_index(const KeyCode key) {
+        return static_cast<size_t>(key);
+    }
+
+}}  // namespace sung::key
