781adebb33a1c3727fd0d7b28938df2f0638b625
Sungmin Woo <woos8899@gmail.com>
2025-01-09 14:39:43 +0900

    Data central implementation

--- source/gameengine/include/sung/gameengine/data_central.hpp
+++ source/gameengine/include/sung/gameengine/data_central.hpp
@@ -1,20 +1,84 @@
 #pragma once
 
 #include <memory>
-#include <mutex>
+#include <optional>
+#include <shared_mutex>
 
 #include "sung/general/threading.hpp"
 
 
 namespace sung {
 
+    class InternalDataSharedPtr;
+
+
     struct IDataAdapter {
         virtual ~IDataAdapter() = default;
+        virtual void load() = 0;
+        virtual void finalize() = 0;
+    };
+
+
+    class DataReader {
+
+    public:
+        using Lock = std::shared_lock<std::shared_mutex>;
+        using ResPair = std::pair<Lock, const IDataAdapter*>;
+        using DataPtr = std::shared_ptr<InternalDataSharedPtr>;
+
+        DataReader() = default;
+        DataReader(DataPtr data);
+        ~DataReader();
+
+        DataReader(DataReader&&) noexcept;
+        DataReader& operator=(DataReader&&) noexcept;
+
+        std::optional<ResPair> get();
+        size_t id() const;
+        bool is_dangling() const;
+
+    private:
+        std::weak_ptr<InternalDataSharedPtr> data_;
+        Lock lock_;
+    };
+
+
+    class DataWriter {
+
+    public:
+        using Lock = std::unique_lock<std::shared_mutex>;
+        using ResPair = std::pair<Lock, IDataAdapter*>;
+        using DataPtr = std::shared_ptr<InternalDataSharedPtr>;
+
+        DataWriter() = default;
+        DataWriter(DataPtr data);
+        ~DataWriter();
+
+        DataWriter(DataWriter&&) noexcept;
+        DataWriter& operator=(DataWriter&&) noexcept;
+
+        std::optional<ResPair> get();
+        size_t id() const;
+        bool is_dangling() const;
+
+    private:
+        std::weak_ptr<InternalDataSharedPtr> data_;
+        Lock lock_;
     };
 
 
     struct IDataCentral {
         virtual ~IDataCentral() = default;
+
+        virtual DataReader add(std::unique_ptr<IDataAdapter> data) = 0;
+
+        virtual std::optional<DataReader> get_reader_of(
+            const DataWriter& accessor
+        ) = 0;
+
+        virtual std::optional<DataWriter> get_writer_of(
+            const DataReader& accessor
+        ) = 0;
     };
 
     std::unique_ptr<IDataCentral> create_data_central(HTaskSche task_sche);
--- source/gameengine/src/data_central.cpp
+++ source/gameengine/src/data_central.cpp
@@ -1,5 +1,21 @@
 #include "sung/gameengine/data_central.hpp"
 
+#include <vector>
+
+
+namespace sung {
+
+    class InternalDataSharedPtr {
+
+    public:
+        size_t id() const { return reinterpret_cast<size_t>(data_.get()); }
+
+        std::shared_mutex mut_;
+        std::unique_ptr<IDataAdapter> data_;
+    };
+
+}  // namespace sung
+
 
 namespace {
 
@@ -8,11 +24,142 @@ namespace {
     public:
         DataCentral(sung::HTaskSche task_sche) : task_sche_{ task_sche } {}
 
+        sung::DataReader add(std::unique_ptr<sung::IDataAdapter> data
+        ) override {
+            auto data_ptr = std::make_shared<sung::InternalDataSharedPtr>();
+            data_ptr->data_ = std::move(data);
+            data_list_.push_back(data_ptr);
+            return sung::DataReader{ data_ptr };
+        }
+
+        std::optional<sung::DataReader> get_reader_of(
+            const sung::DataWriter& accessor
+        ) override {
+            const auto id = accessor.id();
+
+            for (auto& x : data_list_) {
+                if (x->id() == id)
+                    return sung::DataReader{ x };
+            }
+
+            return std::nullopt;
+        }
+
+        std::optional<sung::DataWriter> get_writer_of(
+            const sung::DataReader& accessor
+        ) override {
+            const auto id = accessor.id();
+
+            for (auto& x : data_list_) {
+                if (x->id() == id)
+                    return sung::DataWriter{ x };
+            }
+
+            return std::nullopt;
+        }
+
     private:
+        std::vector<std::shared_ptr<sung::InternalDataSharedPtr>> data_list_;
         sung::HTaskSche task_sche_;
     };
 
 }  // namespace
+
+
+// DataReader
+namespace sung {
+
+    DataReader::DataReader(DataPtr data)
+        : data_(data), lock_(data->mut_, std::defer_lock) {}
+
+    DataReader::~DataReader() {}
+
+    DataReader::DataReader(DataReader&& rhs) noexcept {
+        std::swap(data_, rhs.data_);
+        std::swap(lock_, rhs.lock_);
+    }
+
+    DataReader& DataReader::operator=(DataReader&& rhs) noexcept {
+        std::swap(data_, rhs.data_);
+        std::swap(lock_, rhs.lock_);
+        return *this;
+    }
+
+    std::optional<DataReader::ResPair> DataReader::get() {
+        if (data_.expired())
+            return std::nullopt;
+
+        auto data = data_.lock();
+        if (!data) {
+            data_.reset();
+            return std::nullopt;
+        }
+
+        try {
+            if (!lock_.try_lock())
+                return std::nullopt;
+
+            ResPair out{ std::move(lock_), data->data_.get() };
+            lock_ = Lock{ data->mut_, std::defer_lock };
+            return out;
+        } catch (const std::system_error&) {
+            return std::nullopt;
+        }
+    }
+
+    size_t DataReader::id() const { return data_.lock()->id(); }
+
+    bool DataReader::is_dangling() const { return data_.expired(); }
+
+}  // namespace sung
+
+
+// WriterReader
+namespace sung {
+
+    DataWriter::DataWriter(DataPtr data)
+        : data_(data), lock_(data->mut_, std::defer_lock) {}
+
+    DataWriter::~DataWriter() {}
+
+    DataWriter::DataWriter(DataWriter&& rhs) noexcept {
+        std::swap(data_, rhs.data_);
+        std::swap(lock_, rhs.lock_);
+    }
+
+    DataWriter& DataWriter::operator=(DataWriter&& rhs) noexcept {
+        std::swap(data_, rhs.data_);
+        std::swap(lock_, rhs.lock_);
+        return *this;
+    }
+
+    std::optional<DataWriter::ResPair> DataWriter::get() {
+        if (data_.expired())
+            return std::nullopt;
+
+        auto data = data_.lock();
+        if (!data) {
+            data_.reset();
+            return std::nullopt;
+        }
+
+        try {
+            if (!lock_.try_lock())
+                return std::nullopt;
+
+            ResPair out{ std::move(lock_), data->data_.get() };
+            lock_ = Lock{ data->mut_, std::defer_lock };
+            return out;
+        } catch (const std::system_error&) {
+            return std::nullopt;
+        }
+    }
+
+    size_t DataWriter::id() const { return data_.lock()->id(); }
+
+    bool DataWriter::is_dangling() const { return data_.expired(); }
+
+}  // namespace sung
 
 
 namespace sung {
