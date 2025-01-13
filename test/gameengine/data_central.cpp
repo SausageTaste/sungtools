1396485e7475cbe06602a2408abf302d813aa6ee
Sungmin Woo <woos8899@gmail.com>
2025-01-13 10:12:45 +0900

    Remove test for data cenetral

--- test/gameengine/data_central.cpp
+++ test/gameengine/data_central.cpp
@@ -16,44 +16,7 @@ namespace {
     };
 
 
-    TEST(DataCentral, Test) {
-        auto task_sche = sung::create_task_scheduler();
-        auto datacen = sung::create_data_central(task_sche);
-        ASSERT_NE(datacen, nullptr);
-
-        auto reader = datacen->add(std::make_unique<MockDataAdapter>());
-        auto writer = datacen->get_writer_of(reader);
-        ASSERT_NE(writer, std::nullopt);
-
-        {
-            auto r0 = reader.get();
-            ASSERT_NE(r0, std::nullopt);
-            auto r1 = reader.get();
-            ASSERT_NE(r1, std::nullopt);
-            auto w0 = writer->get();
-            ASSERT_EQ(w0, std::nullopt);
-            auto r2 = reader.get();
-            ASSERT_NE(r2, std::nullopt);
-        }
-
-        {
-            auto w0 = writer->get();
-            ASSERT_NE(w0, std::nullopt);
-            auto r0 = reader.get();
-            ASSERT_EQ(r0, std::nullopt);
-
-            w0->second->load();
-        }
-
-        {
-            auto r0 = reader.get();
-            ASSERT_NE(r0, std::nullopt);
-
-            auto adaptor = dynamic_cast<const MockDataAdapter*>(r0->second);
-            ASSERT_NE(adaptor, nullptr);
-            ASSERT_EQ(adaptor->load_count_, 1);
-        }
-    }
+    TEST(DataCentral, Test) {}
 
 }  // namespace
 
