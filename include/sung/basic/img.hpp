091ab0e5a0719a3d89b5f7b48ba0bda11da73e99
Sungmin Woo <woos8899@gmail.com>
2025-11-26 17:35:03 +0900

    Refactor 3D image classes and add TImageView3D

--- include/sung/basic/img.hpp
+++ include/sung/basic/img.hpp
@@ -7,32 +7,60 @@
 
 namespace sung {
 
-    template <typename DataType>
-    class TImage3D {
+    class ImageMetadata3D {
 
     public:
-        bool alloc(size_t channels, size_t x, size_t y = 1, size_t z = 1) {
+        size_t channels() const { return channels_; }
+        size_t width() const { return x_size_; }
+        size_t height() const { return y_size_; }
+        size_t depth() const { return z_size_; }
+
+        size_t make_texel_idx(size_t x, size_t y, size_t z) const {
+            const auto texel_size = channels_;
+            const auto row_size = texel_size * x_size_;
+            const auto slice_size = row_size * y_size_;
+            return z * slice_size + y * row_size + x * texel_size;
+        }
+
+    protected:
+        void set_size(size_t channels, size_t x, size_t y, size_t z) {
             channels_ = channels;
             x_size_ = x;
             y_size_ = y;
             z_size_ = z;
-            data_.resize(channels_ * width_ * height_ * depth_);
-            return true;
         }
 
         void clear() {
-            data_.clear();
             channels_ = 0;
             x_size_ = 0;
             y_size_ = 0;
             z_size_ = 0;
         }
 
-        size_t channels() const override { return channels_; }
-        size_t width() const override { return x_size_; }
-        size_t height() const override { return y_size_; }
-        size_t depth() const override { return z_size_; }
-        size_t value_type_size() const override { return sizeof(DataType); }
+    private:
+        size_t channels_ = 0;
+        size_t x_size_ = 0;
+        size_t y_size_ = 0;
+        size_t z_size_ = 0;
+    };
+
+
+    template <typename DataType>
+    class TImage3D : public ImageMetadata3D {
+
+    public:
+        bool alloc(size_t channels, size_t x, size_t y = 1, size_t z = 1) {
+            this->set_size(channels, x, y, z);
+            data_.resize(channels_ * x * y * z);
+            return true;
+        }
+
+        void clear() {
+            data_.clear();
+            this->ImageMetadata3D::clear();
+        }
+
+        size_t value_type_size() const { return sizeof(DataType); }
 
         DataType* texel_ptr(size_t x, size_t y = 0, size_t z = 0) {
             const auto index = this->make_texel_idx(x, y, z);
@@ -48,20 +76,42 @@ namespace sung {
             return reinterpret_cast<const byte8*>(data_.data());
         }
 
-        size_t buffer_size() const { return data_.size() * sizeof(T); }
-
-        size_t make_texel_idx(size_t x, size_t y, size_t z) const {
-            const auto texel_size = channels_;
-            const auto row_size = texel_size * x_size_;
-            const auto slice_size = row_size * y_size_;
-            return z * slice_size + y * row_size + x * texel_size;
-        }
-
         std::vector<DataType> data_;
         size_t channels_ = 0;
         size_t x_size_ = 0;
         size_t y_size_ = 0;
         size_t z_size_ = 0;
+    };
+
+
+    template <typename DataType>
+    class TImageView3D : public ImageMetadata3D {
+
+    public:
+        void set(
+            const DataType* data, size_t channels, size_t x, size_t y, size_t z
+        ) {
+            data_ = data;
+            this->set_size(channels, x, y, z);
+        }
+
+        void clear() {
+            data_ = nullptr;
+            this->ImageMetadata3D::clear();
+        }
+
+        size_t value_type_size() const { return sizeof(DataType); }
+
+        const DataType* texel_ptr(size_t x, size_t y = 0, size_t z = 0) const {
+            const auto index = this->make_texel_idx(x, y, z);
+            return data_ + index;
+        }
+
+        const byte8* buffer_data() const {
+            return reinterpret_cast<const byte8*>(data_);
+        }
+
+        const DataType* data_ = nullptr;
     };
 
 }  // namespace sung
