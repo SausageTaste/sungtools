7ec9ad55a6f4fcf4affe5c8858c26317048346e2
Sungmin Woo <woos8899@gmail.com>
2025-12-18 17:48:31 +0900

    `Image2D` class

---
+++ include/sung/basic/img2d.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <vector>
+
+#include "sung/basic/bytes.hpp"
+
+
+namespace sung {
+
+    enum class ImageScalarType {
+        uint8,
+        float16,
+        float32,
+        float64,
+    };
+
+    size_t get_image_scalar_type_bytes(ImageScalarType type);
+
+
+    class Image2D {
+
+    public:
+        void clear();
+        void resize_data_to_fit();
+
+        size_t scalar_bytes() const;
+        size_t pixel_bytes() const;
+        size_t row_bytes() const;
+
+        byte8* pixel_ptr(uint32_t idx);
+        byte8* pixel_ptr(uint32_t x, uint32_t y);
+        const byte8* pixel_ptr(uint32_t idx) const;
+        const byte8* pixel_ptr(uint32_t x, uint32_t y) const;
+
+        template <typename T>
+        T* pixel_ptr(uint32_t idx) {
+            return reinterpret_cast<T*>(this->pixel_ptr(idx));
+        }
+
+        template <typename T>
+        T* pixel_ptr(uint32_t x, uint32_t y) {
+            return reinterpret_cast<T*>(this->pixel_ptr(x, y));
+        }
+
+        template <typename T>
+        const T* pixel_ptr(uint32_t idx) const {
+            return reinterpret_cast<const T*>(this->pixel_ptr(idx));
+        }
+
+        template <typename T>
+        const T* pixel_ptr(uint32_t x, uint32_t y) const {
+            return reinterpret_cast<const T*>(this->pixel_ptr(x, y));
+        }
+
+    public:
+        std::vector<byte8> data_;
+        ImageScalarType scalar_type_ = ImageScalarType::uint8;
+        size_t channels_ = 0;
+        size_t x_size_ = 0;
+        size_t y_size_ = 0;
+    };
+
+}  // namespace sung
---
+++ src/basic/img2d.cpp
@@ -0,0 +1,65 @@
+#include "sung/basic/img2d.hpp"
+
+
+namespace sung {
+
+    void Image2D::clear() {
+        data_.clear();
+        x_size_ = 0;
+        y_size_ = 0;
+        channels_ = 0;
+        scalar_type_ = ImageScalarType::uint8;
+    }
+
+    void Image2D::resize_data_to_fit() {
+        const auto size = y_size_ * this->row_bytes();
+        data_.resize(size);
+    }
+
+    size_t Image2D::scalar_bytes() const {
+        return get_image_scalar_type_bytes(scalar_type_);
+    }
+
+    size_t Image2D::pixel_bytes() const {
+        return channels_ * this->scalar_bytes();
+    }
+
+    size_t Image2D::row_bytes() const { return x_size_ * this->pixel_bytes(); }
+
+    byte8* Image2D::pixel_ptr(uint32_t idx) {
+        return data_.data() + idx * this->pixel_bytes();
+    }
+
+    byte8* Image2D::pixel_ptr(uint32_t x, uint32_t y) {
+        return this->pixel_ptr(y * this->row_bytes() + x);
+    }
+
+    const byte8* Image2D::pixel_ptr(uint32_t idx) const {
+        return data_.data() + idx * this->pixel_bytes();
+    }
+
+    const byte8* Image2D::pixel_ptr(uint32_t x, uint32_t y) const {
+        return this->pixel_ptr(y * this->row_bytes() + x);
+    }
+
+}  // namespace sung
+
+
+namespace sung {
+
+    size_t get_image_scalar_type_bytes(ImageScalarType type) {
+        switch (type) {
+            case ImageScalarType::uint8:
+                return 1;
+            case ImageScalarType::float16:
+                return 2;
+            case ImageScalarType::float32:
+                return 4;
+            case ImageScalarType::float64:
+                return 8;
+            default:
+                return 0;
+        }
+    }
+
+}  // namespace sung
