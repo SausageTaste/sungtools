ed90183e3901706c4a4b180c19f88b4b97eb619a
Sungmin Woo <woos8899@gmail.com>
2025-12-24 14:37:01 +0900

    `fetch_clamp`

--- include/sung/basic/sample2d.hpp
+++ include/sung/basic/sample2d.hpp
@@ -9,19 +9,12 @@
 namespace sung {
 
     template <size_t Channels, typename T>
-    std::array<T, Channels> sample_nearest_clamp(
+    std::array<T, Channels> fetch_clamp(
         const TImageView2D<T>& view, double x, double y
     ) {
-        const auto x_texel = x * static_cast<double>(view.x_size());
-        const auto y_texel = y * static_cast<double>(view.y_size());
-
-        const auto x_nearest = sung::clamp<double>(
-            std::floor(x_texel), 0, view.x_size() - 1
-        );
-        const auto y_nearest = sung::clamp<double>(
-            std::floor(y_texel), 0, view.y_size() - 1
-        );
-        auto pixel_ptr = view.pixel_ptr(
+        const auto x_nearest = sung::clamp<double>(x, 0, view.x_size() - 1);
+        const auto y_nearest = sung::clamp<double>(y, 0, view.y_size() - 1);
+        const auto pixel_ptr = view.pixel_ptr(
             static_cast<size_t>(x_nearest), static_cast<size_t>(y_nearest)
         );
 
@@ -30,6 +23,18 @@ namespace sung {
             result[c] = pixel_ptr[c];
         }
         return result;
+    }
+
+    template <size_t Channels, typename T>
+    std::array<T, Channels> sample_nearest_clamp(
+        const TImageView2D<T>& view, double x, double y
+    ) {
+        const auto x_texel = x * static_cast<double>(view.x_size());
+        const auto y_texel = y * static_cast<double>(view.y_size());
+
+        return fetch_clamp<Channels, T>(
+            view, std::floor(x_texel), std::floor(y_texel)
+        );
     }
 
 }  // namespace sung
