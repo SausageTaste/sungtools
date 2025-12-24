4387c6e74ab55e304a7b3d1aaee6586cf355f89f
Sungmin Woo <woos8899@gmail.com>
2025-12-24 13:51:06 +0900

    `ImageMetadata2D`

--- include/sung/basic/img2d.hpp
+++ include/sung/basic/img2d.hpp
@@ -7,35 +7,101 @@
 
 namespace sung {
 
-    enum class ImageScalarType {
-        uint8,
-        float16,
-        float32,
-        float64,
+    class ImageMetadata2D {
+
+    public:
+        void set_scalar_bytes(const size_t scalar_bytes);
+        void set_channels(const size_t channels);
+        void set_x_size(const size_t x);
+        void set_y_size(const size_t y);
+        void set_xy_size(const size_t x, const size_t y);
+        void set_xy_size(const size_t xy);
+
+        void set_metadata(
+            const size_t scalar_bytes,
+            const size_t channels,
+            const size_t x,
+            const size_t y,
+            const size_t row_padding = 0
+        );
+
+        template <typename T>
+        void set_metadata(
+            const size_t channels,
+            const size_t x,
+            const size_t y,
+            const size_t row_padding = 0
+        ) {
+            this->set_metadata(sizeof(T), channels, x, y, row_padding);
+        }
+
+        size_t channels() const;
+        size_t x_size() const;
+        size_t y_size() const;
+
+        size_t scalar_bytes() const;
+        size_t padding_bytes() const;
+
+        size_t pixel_bytes() const;
+        size_t row_bytes() const;
+        size_t size_bytes() const;
+
+        bool check_idx(size_t x, size_t y) const;
+
+    protected:
+        void clear_metadata();
+        size_t make_texel_idx(size_t x, size_t y) const;
+        size_t make_texel_idx_clamp(size_t x, size_t y) const;
+
+    private:
+        size_t scalar_bytes_ = 0;
+        size_t channels_ = 0;
+        size_t x_size_ = 0;
+        size_t y_size_ = 0;
+        size_t row_padding_ = 0;
     };
 
-    size_t get_image_scalar_type_bytes(ImageScalarType type);
+
+    template <typename DataType>
+    class TImageView2D : public ImageMetadata2D {
+
+    public:
+        void set(
+            const DataType* data,
+            size_t channels,
+            size_t x,
+            size_t y,
+            size_t row_padding = 0
+        ) {
+            data_ = data;
+            this->set_metadata<DataType>(channels, x, y, row_padding);
+        }
+
+        void clear() {
+            data_ = nullptr;
+            this->clear_metadata();
+        }
+
+        const DataType* pixel_ptr(size_t x, size_t y) const {
+            const auto index = this->make_texel_idx_clamp(x, y);
+            return reinterpret_cast<const DataType*>(
+                reinterpret_cast<const byte8*>(data_) + index
+            );
+        }
+
+    private:
+        const DataType* data_ = nullptr;
+    };
 
 
-    class Image2D {
+    class Image2D : public ImageMetadata2D {
 
     public:
         void clear();
         void resize_data_to_fit();
 
-        size_t scalar_bytes() const;
-        size_t pixel_bytes() const;
-        size_t row_bytes() const;
-
-        byte8* pixel_ptr(size_t idx);
         byte8* pixel_ptr(size_t x, size_t y);
-        const byte8* pixel_ptr(size_t idx) const;
         const byte8* pixel_ptr(size_t x, size_t y) const;
-
-        template <typename T>
-        T* pixel_ptr(size_t idx) {
-            return reinterpret_cast<T*>(this->pixel_ptr(idx));
-        }
 
         template <typename T>
         T* pixel_ptr(size_t x, size_t y) {
@@ -43,21 +109,25 @@ namespace sung {
         }
 
         template <typename T>
-        const T* pixel_ptr(size_t idx) const {
-            return reinterpret_cast<const T*>(this->pixel_ptr(idx));
-        }
-
-        template <typename T>
         const T* pixel_ptr(size_t x, size_t y) const {
             return reinterpret_cast<const T*>(this->pixel_ptr(x, y));
         }
 
+        template <typename T>
+        TImageView2D<T> make_view() const {
+            TImageView2D<T> view;
+            view.set(
+                reinterpret_cast<const T*>(data_.data()),
+                this->channels(),
+                this->x_size(),
+                this->y_size(),
+                this->padding_bytes()
+            );
+            return view;
+        }
+
     public:
         std::vector<byte8> data_;
-        ImageScalarType scalar_type_ = ImageScalarType::uint8;
-        size_t channels_ = 0;
-        size_t x_size_ = 0;
-        size_t y_size_ = 0;
     };
 
 }  // namespace sung
--- src/basic/img2d.cpp
+++ src/basic/img2d.cpp
@@ -1,45 +1,96 @@
 #include "sung/basic/img2d.hpp"
 
+#include "sung/basic/mamath.hpp"
 
+
+// ImageMetadata2D
 namespace sung {
 
-    void Image2D::clear() {
-        data_.clear();
+    void ImageMetadata2D::set_scalar_bytes(const size_t scalar_bytes) {
+        scalar_bytes_ = scalar_bytes;
+    }
+
+    void ImageMetadata2D::set_channels(const size_t channels) {
+        channels_ = channels;
+    }
+
+    void ImageMetadata2D::set_x_size(const size_t x) { x_size_ = x; }
+
+    void ImageMetadata2D::set_y_size(const size_t y) { y_size_ = y; }
+
+    void ImageMetadata2D::set_xy_size(const size_t x, const size_t y) {
+        this->set_x_size(x);
+        this->set_y_size(y);
+    }
+
+    void ImageMetadata2D::set_xy_size(const size_t xy) {
+        this->set_x_size(xy);
+        this->set_y_size(xy);
+    }
+
+    void ImageMetadata2D::set_metadata(
+        const size_t scalar_bytes,
+        const size_t channels,
+        const size_t x,
+        const size_t y,
+        const size_t row_padding
+    ) {
+        scalar_bytes_ = scalar_bytes;
+        channels_ = channels;
+        x_size_ = x;
+        y_size_ = y;
+        row_padding_ = row_padding;
+    }
+
+    size_t ImageMetadata2D::channels() const { return channels_; }
+
+    size_t ImageMetadata2D::x_size() const { return x_size_; }
+
+    size_t ImageMetadata2D::y_size() const { return y_size_; }
+
+    size_t ImageMetadata2D::scalar_bytes() const { return scalar_bytes_; }
+
+    size_t ImageMetadata2D::padding_bytes() const { return row_padding_; }
+
+    size_t ImageMetadata2D::pixel_bytes() const {
+        return channels_ * scalar_bytes_;
+    }
+
+    size_t ImageMetadata2D::row_bytes() const {
+        return x_size_ * this->pixel_bytes() + row_padding_;
+    }
+
+    size_t ImageMetadata2D::size_bytes() const {
+        return y_size_ * this->row_bytes();
+    }
+
+    bool ImageMetadata2D::check_idx(size_t x, size_t y) const {
+        if (x >= x_size_)
+            return false;
+        if (y >= y_size_)
+            return false;
+        return true;
+    }
+
+    void ImageMetadata2D::clear_metadata() {
+        scalar_bytes_ = 0;
+        channels_ = 0;
         x_size_ = 0;
         y_size_ = 0;
-        channels_ = 0;
-        scalar_type_ = ImageScalarType::uint8;
+        row_padding_ = 0;
     }
 
-    void Image2D::resize_data_to_fit() {
-        const auto size = y_size_ * this->row_bytes();
-        data_.resize(size);
+    size_t ImageMetadata2D::make_texel_idx(size_t x, size_t y) const {
+        const auto pixel_bytes = scalar_bytes_ * channels_;
+        const auto row_bytes = pixel_bytes * x_size_ + row_padding_;
+        return y * row_bytes + x * pixel_bytes;
     }
 
-    size_t Image2D::scalar_bytes() const {
-        return get_image_scalar_type_bytes(scalar_type_);
-    }
-
-    size_t Image2D::pixel_bytes() const {
-        return channels_ * this->scalar_bytes();
-    }
-
-    size_t Image2D::row_bytes() const { return x_size_ * this->pixel_bytes(); }
-
-    byte8* Image2D::pixel_ptr(size_t idx) {
-        return data_.data() + idx * this->pixel_bytes();
-    }
-
-    byte8* Image2D::pixel_ptr(size_t x, size_t y) {
-        return this->pixel_ptr(y * x_size_ + x);
-    }
-
-    const byte8* Image2D::pixel_ptr(size_t idx) const {
-        return data_.data() + idx * this->pixel_bytes();
-    }
-
-    const byte8* Image2D::pixel_ptr(size_t x, size_t y) const {
-        return this->pixel_ptr(y * x_size_ + x);
+    size_t ImageMetadata2D::make_texel_idx_clamp(size_t x, size_t y) const {
+        return this->make_texel_idx(
+            sung::clamp<size_t>(x, 0, x_size_ - 1),
+            sung::clamp<size_t>(y, 0, y_size_ - 1)
+        );
     }
 
 }  // namespace sung
@@ -47,19 +98,19 @@ namespace sung {
 
 namespace sung {
 
-    size_t get_image_scalar_type_bytes(ImageScalarType type) {
-        switch (type) {
-            case ImageScalarType::uint8:
-                return 1;
-            case ImageScalarType::float16:
-                return 2;
-            case ImageScalarType::float32:
-                return 4;
-            case ImageScalarType::float64:
-                return 8;
-            default:
-                return 0;
-        }
+    void Image2D::clear() {
+        data_.clear();
+        this->clear_metadata();
+    }
+
+    void Image2D::resize_data_to_fit() { data_.resize(this->size_bytes()); }
+
+    byte8* Image2D::pixel_ptr(size_t x, size_t y) {
+        return data_.data() + this->make_texel_idx(x, y);
+    }
+
+    const byte8* Image2D::pixel_ptr(size_t x, size_t y) const {
+        return data_.data() + this->make_texel_idx(x, y);
     }
 
 }  // namespace sung
